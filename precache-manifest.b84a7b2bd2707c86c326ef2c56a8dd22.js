1a7e705e62aa7b9de8bd74aaecc66c5eca5c05f3
AndrewIpatov <ipatov123321@gmail.com>
2020-06-10 19:04:36 +0300

    Updates

---
+++ precache-manifest.b84a7b2bd2707c86c326ef2c56a8dd22.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab46f40d26bacf04794e504df129d3f4",
+    "url": "/pizza-test-app/index.html"
+  },
+  {
+    "revision": "31be7d54b0870d1638d3",
+    "url": "/pizza-test-app/static/css/2.610bbd32.chunk.css"
+  },
+  {
+    "revision": "fc6e020a7b1d5212a9e5",
+    "url": "/pizza-test-app/static/css/main.9f1f15fb.chunk.css"
+  },
+  {
+    "revision": "31be7d54b0870d1638d3",
+    "url": "/pizza-test-app/static/js/2.412376a3.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/pizza-test-app/static/js/2.412376a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc6e020a7b1d5212a9e5",
+    "url": "/pizza-test-app/static/js/main.ef2fb425.chunk.js"
+  },
+  {
+    "revision": "f9d91553f39cb252cccd",
+    "url": "/pizza-test-app/static/js/runtime-main.944af01b.js"
+  },
+  {
+    "revision": "9d5146dd33d554e0700a3c3c8ac94b78",
+    "url": "/pizza-test-app/static/media/galleryPhotoEight.9d5146dd.jpg"
+  },
+  {
+    "revision": "4be54bdb0500001fdf4d9864c90ca40c",
+    "url": "/pizza-test-app/static/media/galleryPhotoEleven.4be54bdb.jpg"
+  },
+  {
+    "revision": "204750a6e088d438b5a47c9c9107fea9",
+    "url": "/pizza-test-app/static/media/galleryPhotoFive.204750a6.jpg"
+  },
+  {
+    "revision": "3a2f7ec62597bace0c8e5086dc06caa5",
+    "url": "/pizza-test-app/static/media/galleryPhotoFour.3a2f7ec6.jpg"
+  },
+  {
+    "revision": "da98787f284ed62d94583eaaa370c3c6",
+    "url": "/pizza-test-app/static/media/galleryPhotoNine.da98787f.jpg"
+  },
+  {
+    "revision": "75f81eb1b74072f1dc0cbb31e5196eab",
+    "url": "/pizza-test-app/static/media/galleryPhotoOne.75f81eb1.jpg"
+  },
+  {
+    "revision": "2bc87d3153c80f3ca3dba923d771031c",
+    "url": "/pizza-test-app/static/media/galleryPhotoSeven.2bc87d31.jpg"
+  },
+  {
+    "revision": "e9d4027259d297ec334e052166d8293e",
+    "url": "/pizza-test-app/static/media/galleryPhotoSix.e9d40272.jpg"
+  },
+  {
+    "revision": "8d60a9e122c377fa5c14ee684b7af8ff",
+    "url": "/pizza-test-app/static/media/galleryPhotoTen.8d60a9e1.jpg"
+  },
+  {
+    "revision": "e1e1fa6dd0cd2504241bb1ad19cb1e63",
+    "url": "/pizza-test-app/static/media/galleryPhotoThree.e1e1fa6d.jpg"
+  },
+  {
+    "revision": "7ad76d90d924b6b1a77b065433290f2c",
+    "url": "/pizza-test-app/static/media/galleryPhotoTwelve.7ad76d90.jpg"
+  },
+  {
+    "revision": "b4eaeaf213ec914dfa9bd0a3ba4e076f",
+    "url": "/pizza-test-app/static/media/galleryPhotoTwo.b4eaeaf2.jpg"
+  },
+  {
+    "revision": "2eb89907ae804e8c63aeb812af0b898b",
+    "url": "/pizza-test-app/static/media/homePhotoFour.2eb89907.jpg"
+  },
+  {
+    "revision": "14014001b7df0251d822e7f7c4652875",
+    "url": "/pizza-test-app/static/media/homePhotoOne.14014001.jpg"
+  },
+  {
+    "revision": "65b5d88cbb2ecc0113fc7615a0a5a850",
+    "url": "/pizza-test-app/static/media/homePhotoThree.65b5d88c.jpg"
+  },
+  {
+    "revision": "c9ec62943446af2640c2d1cb039b1d45",
+    "url": "/pizza-test-app/static/media/homePhotoTwo.c9ec6294.jpg"
+  },
+  {
+    "revision": "8255ff9e79853065cf301e3a6da33faf",
+    "url": "/pizza-test-app/static/media/logo.8255ff9e.png"
+  },
+  {
+    "revision": "c50be105585d057492509c4df35f21f4",
+    "url": "/pizza-test-app/static/media/slideHomeOne.c50be105.jpg"
+  },
+  {
+    "revision": "d01dcd205ca2753075e4c2874fa1e94a",
+    "url": "/pizza-test-app/static/media/slideHomeThree.d01dcd20.jpg"
+  },
+  {
+    "revision": "1f3f4bed909380ca6c470614cee8b533",
+    "url": "/pizza-test-app/static/media/slideHomeTwo.1f3f4bed.jpg"
+  }
+]);
